e9b81a8f7d31ca2549ec13f1fee1a5affeb91752
djsereno <djsereno91@gmail.com>
2023-08-09 23:44:13 -0700

    Add Input and Section components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import PropTypes from 'prop-types';
 
-function App() {
-  const [count, setCount] = useState(0)
+const sections = [
+  {
+    id: 0,
+    heading: 'General',
+    inputLabels: ['First Name', 'Last Name', 'Email', 'Phone', 'Location'],
+  },
+  {
+    id: 1,
+    heading: 'Education',
+    inputLabels: ['School Name', 'Location', 'Degree', 'Start Date', 'End Date'],
+  },
+  {
+    id: 2,
+    heading: 'Work Experience',
+    inputLabels: ['Company Name', 'Location', 'Position', 'Start Date', 'End Date', 'Responsibilities'],
+  },
+];
+
+function Input({ label }) {
+  return (
+    <label>
+      {`${label} : `}
+      <input type="text"></input>
+    </label>
+  );
+}
+Input.propTypes = {
+  label: PropTypes.string.isRequired,
+};
+
+function Section({ section }) {
+  const { id, heading, inputLabels } = section;
+  const inputs = inputLabels.map((label) => <Input label={label} />);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <section id={id}>
+      <h2>{heading}</h2>
+      {inputs}
+    </section>
+  );
 }
 
-export default App
+function App() {
+  const sectionComps = sections.map((section) => <Section section={section} />);
+  return <>{sectionComps}</>;
+}
+
+export default App;
