9079e1e7576969ade00301ee0fe76c7b8417ad3a
djsereno <djsereno91@gmail.com>
2023-08-10 23:37:59 -0700

    Add form buttons, move form data to separate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,38 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react';
+import { data } from './data';
 import './App.css';
-import PropTypes from 'prop-types';
 
-const sections = [
-  {
-    id: 0,
-    heading: 'General',
-    inputLabels: ['First Name', 'Last Name', 'Email', 'Phone', 'Location'],
-  },
-  {
-    id: 1,
-    heading: 'Education',
-    inputLabels: ['School Name', 'Location', 'Degree', 'Start Date', 'End Date'],
-  },
-  {
-    id: 2,
-    heading: 'Work Experience',
-    inputLabels: ['Company Name', 'Location', 'Position', 'Start Date', 'End Date', 'Responsibilities'],
-  },
-];
-
-function Input({ label }) {
+function Input({ label, value }) {
   return (
     <label>
       {`${label} : `}
-      <input type="text"></input>
+      <input type="text" value={value} readOnly></input>
     </label>
   );
 }
-Input.propTypes = {
-  label: PropTypes.string.isRequired,
-};
 
 function Section({ section }) {
-  const { id, heading, inputLabels } = section;
-  const inputs = inputLabels.map((label) => <Input label={label} />);
+  const { id, heading, inputs } = section;
+  const inputsFields = inputs.map((input) => <Input key={input.label} label={input.label} value={input.value} />);
 
   return (
     <section id={id}>
       <h2>{heading}</h2>
-      {inputs}
+      {inputsFields}
+      <button type="button">{section.isSubmitted ? 'Edit' : 'Submit'}</button>
     </section>
   );
 }
 
 function App() {
-  const sectionComps = sections.map((section) => <Section section={section} />);
-  return <>{sectionComps}</>;
+  return (
+    <>
+      {data.map((section) => (
+        <Section key={section.heading} section={section} />
+      ))}
+    </>
+  );
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,87 @@
+export const data = [
+  {
+    id: 0,
+    heading: 'General',
+    inputs: [
+      {
+        label: 'First Name',
+        value: 'John',
+      },
+      {
+        label: 'Last Name',
+        value: 'Smith',
+      },
+      {
+        label: 'Email',
+        value: 'johnsmith123@gmail.com',
+      },
+      {
+        label: 'Phone',
+        value: '(555) 123-4567',
+      },
+      {
+        label: 'Location',
+        value: 'Springfield, IL',
+      },
+    ],
+    isSubmitted: false,
+  },
+  {
+    id: 1,
+    heading: 'Education',
+    inputs: [
+      {
+        label: 'School Name',
+        value: 'Harvard',
+      },
+      {
+        label: 'Location',
+        value: 'Cambridge, MA',
+      },
+      {
+        label: 'Degree',
+        value: 'Culinary Arts',
+      },
+      {
+        label: 'Start Date',
+        value: '2010',
+      },
+      {
+        label: 'End Date',
+        value: '2014',
+      },
+    ],
+    isSubmitted: false,
+  },
+  {
+    id: 2,
+    heading: 'Work Experience',
+    inputs: [
+      {
+        label: 'Company Name',
+        value: 'Burger King',
+      },
+      {
+        label: 'Location',
+        value: 'Boston, MA',
+      },
+      {
+        label: 'Position',
+        value: 'Head Chef',
+      },
+      {
+        label: 'Start Date',
+        value: '2014',
+      },
+      {
+        label: 'End Date',
+        value: '2020',
+      },
+      {
+        label: 'Responsibilities',
+        value: 'Use the fryer',
+      },
+    ],
+    isSubmitted: false,
+  },
+];
