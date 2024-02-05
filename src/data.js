9d3d7e44cbd6b53d1a546f4a412dea2bda65d7b9
djsereno <djsereno91@gmail.com>
2024-02-05 00:03:28 -0800

    Restructure form data structure

--- src/data.js
+++ src/data.js
@@ -2,103 +2,119 @@ export const data = [
   {
     id: 'general',
     label: 'General',
-    inputs: [
+    data: [
       {
-        id: 'firstName',
+        // id: 'firstName',
         label: 'First Name',
         placeholder: 'John',
-        type: 'text'
+        type: 'text',
+        value: ''
       },
       {
-        id: 'lastName',
+        // id: 'lastName',
         label: 'Last Name',
         placeholder: 'Smith',
-        type: 'text'
+        type: 'text',
+        value: ''
       },
       {
-        id: 'email',
+        // id: 'email',
         label: 'Email',
         placeholder: 'johnsmith123@gmail.com',
-        type: 'email'
+        type: 'email',
+        value: ''
       },
       {
-        id: 'phone',
+        // id: 'phone',
         label: 'Phone',
         placeholder: '(555) 123-4567',
-        type: 'tel'
+        type: 'tel',
+        value: ''
       },
       {
-        id: 'location',
+        // id: 'location',
         label: 'Location',
         placeholder: 'Springfield, IL',
-        type: 'text'
+        type: 'text',
+        value: ''
       }
     ]
   },
   {
     id: 'education',
     label: 'Education',
-    inputs: [
+    data: [
       {
         label: 'School Name',
         placeholder: 'Harvard',
-        type: 'text'
+        type: 'text',
+        value: ''
       },
       {
         label: 'Location',
         placeholder: 'Cambridge, MA',
-        type: 'text'
+        type: 'text',
+        value: ''
       },
       {
         label: 'Degree',
         placeholder: 'Culinary Arts',
-        type: 'text'
+        type: 'text',
+        value: ''
       },
       {
         label: 'Start Date',
         placeholder: '2010',
-        type: 'number'
+        type: 'number',
+        value: ''
       },
       {
         label: 'End Date',
         placeholder: '2014',
-        type: 'number'
+        type: 'number',
+        value: ''
       }
     ]
   },
   {
     id: 'workExperience',
     label: 'Work Experience',
-    inputs: [
+    data: [
       {
         label: 'Company Name',
         placeholder: 'Burger King',
-        type: 'text'
+        type: 'text',
+        value: ''
       },
       {
         label: 'Location',
         placeholder: 'Boston, MA',
-        type: 'text'
+        type: 'text',
+        value: ''
       },
       {
         label: 'Position',
         placeholder: 'Head Chef',
-        type: 'text'
+        type: 'text',
+        value: ''
       },
       {
         label: 'Start Date',
         placeholder: '2014',
-        type: 'number'
+        type: 'number',
+        value: ''
       },
       {
         label: 'End Date',
         placeholder: '2020',
-        type: 'number'
+        type: 'number',
+        value: ''
       },
       {
         label: 'Responsibilities',
         placeholder: 'Use the fryer',
-        type: 'textarea'
+        type: 'textarea',
+        value: ''
       }
     ]
   }
