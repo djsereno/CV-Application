7985f3847faf8e967bf5b98fff9bee2584cbb731
djsereno <djsereno91@gmail.com>
2024-03-01 18:51:45 -0800

    Update the example data

--- src/exampledata.js
+++ src/exampledata.js
@@ -1,45 +1,56 @@
 export const exampleData = {
-  "general": [
+  general: [
     {
-      "location": "Springfield, IL",
-      "phone": "(555) 123-4567",
-      "email": "emily.johnson@email.com",
-      "lastName": "Johnson",
-      "firstName": "Emily"
+      location: 'Springfield, IL',
+      phone: '(555) 123-4567',
+      email: 'emily.johnson@email.com',
+      lastName: 'Johnson',
+      firstName: 'Emily'
     }
   ],
-  "education": [
+  education: [
     {
-      "endDate": "2018-05-01",
-      "startDate": "2016-09-01",
-      "location": "Urbana, IL",
-      "degree": "M.S. Computer Science",
-      "schoolName": "University of Illinois at Urbana-Champaign"
+      endDate: '2018-06-15',
+      startDate: '2016-09-15',
+      location: 'Urbana, IL',
+      degree: 'M.S. Computer Science',
+      schoolName: 'University of Illinois at Urbana-Champaign'
     },
     {
-      "endDate": "2016-05-01",
-      "startDate": "2012-09-01",
-      "location": "Evanston, IL",
-      "degree": "B.S. Computer Engineering",
-      "schoolName": "Northwestern University"
+      endDate: '2016-06-15',
+      startDate: '2012-09-15',
+      location: 'Evanston, IL',
+      degree: 'B.S. Computer Engineering',
+      schoolName: 'Northwestern University'
     }
   ],
-  "workExperience": [
+  workExperience: [
     {
-      "duties": "- Led the development of a customer portal, increasing user engagement by 40%.\n- Implemented and maintained RESTful APIs for seamless integration with third-party services.\n- Collaborated with cross-functional teams to design and implement new features for a cloud-based application.",
-      "endDate": "2021-02-26",
-      "startDate": "2018-06-01",
-      "location": "Chicago, IL",
-      "jobTitle": "Senior Software Developer",
-      "companyName": "Tech Solutions Inc."
+      duties:
+        '- Collaborated in the development of a data visualization tool using D3.js and Python.\n- Assisted in maintaining and optimizing backend APIs for enhanced performance.\n- Participated in daily stand-ups and sprint planning meetings to ensure project milestones were met.\n- Conducted code reviews, providing feedback to ensure code quality and adherence to best practices.\n- Worked on the integration of machine learning algorithms into the data processing pipeline.\n- Contributed to the creation of user documentation and training materials.\n- Engaged in continuous learning, staying updated on relevant technologies and industry trends.\n- Supported the troubleshooting and resolution of production issues.',
+      endDate: '2024-01-15',
+      startDate: '2021-03-15',
+      location: 'New York, NY',
+      jobTitle: 'Software Developer',
+      companyName: 'Data Insights Co.'
     },
     {
-      "duties": "- Contributed to the development of a real-time messaging platform using React and Node.js.\n- Conducted code reviews and mentored junior developers, promoting best coding practices.\n- Designed and optimized SQL database queries for improved application performance.",
-      "endDate": "2018-05-31",
-      "startDate": "2016-05-01",
-      "location": "Urbana, IL",
-      "jobTitle": "Software Engineer",
-      "companyName": "Innovate Tech Solutions"
+      duties:
+        '- Led the development of a customer portal, increasing user engagement by 40%.\n- Implemented and maintained RESTful APIs for seamless integration with third-party services.\n- Collaborated with cross-functional teams on new features for a cloud-based application.\n- Designed and implemented a user analytics system within the customer portal.\n- Established and maintained security measures within RESTful APIs.\n- Orchestrated cross-functional workshops for requirements gathering and feedback.\n- Participated in the recruitment process, conducting technical interviews and assessments.',
+      endDate: '2021-02-15',
+      startDate: '2018-06-15',
+      location: 'Chicago, IL',
+      jobTitle: 'Senior Software Developer',
+      companyName: 'Tech Solutions Inc.'
+    },
+    {
+      duties:
+        '- Contributed to the development of a real-time messaging platform using React and Node.js.\n- Conducted code reviews and mentored junior developers.\n- Designed and optimized SQL database queries for improved performance.\n- Architected a modular plugin system for the messaging platform.\n- Led a bi-weekly "Tech Talk" series, fostering knowledge sharing.\n- Implemented automated testing procedures to reduce bugs.\n- Collaborated on technical documentation and tutorials for external developers.\n- Participated in industry conferences and meetups for networking and trend awareness.',
+      endDate: '2018-05-15',
+      startDate: '2016-09-15',
+      location: 'Urbana, IL',
+      jobTitle: 'Software Engineer',
+      companyName: 'Innovate Tech Solutions'
     }
   ]
-}
+};
