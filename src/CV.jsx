6355186822bbf11b4e7aef22f4e0fe541b43d298
Derek Sereno <djsereno91@gmail.com>
2024-03-05 23:57:38 -0800

    Add styling to contain dropdown container

--- src/CV.jsx
+++ src/CV.jsx
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 import General from './General';
 import Education from './Education';
 import Work from './Work';
-import './CV.scss';
 
 function CvPage({ general, education, workExperience }) {
   return (
--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Subsection from './Subsection';
-import './Dropdown.css';
 
 function Dropdown({
   formFields,
